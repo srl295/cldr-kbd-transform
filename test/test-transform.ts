a30d386cae2b59d112881d8bcc1247bb184b522b
Steven R. Loomis <srl295@gmail.com>
2023-03-02 22:35:38 -0600

    chore: ci and unit test

--- test/test-transform.ts
+++ test/test-transform.ts
@@ -1,47 +1,57 @@
 import assert from 'node:assert';
 import test from 'node:test';
+import fs from 'node:fs';
+import processTransform from '../src/lib/transform.js';
 
-test('synchronous passing test', (t) => {
-    // This test passes because it does not throw an exception.
-    assert.strictEqual(1, 1);
-  });
+const sample0 = fs.readFileSync('./test/sample0.xml', 'utf-8');
 
-  test('synchronous failing test', (t) => {
-    // This test fails because it throws an exception.
-    assert.strictEqual(1, 2);
-  });
+test('basic test', (t) => {
+  const inTxt = 'Quack: a big happy string';
+  const outTxt = processTransform(sample0, inTxt);
+  assert.strictEqual(outTxt, `\\\\\\"ubck: b big hbppy string`);
+});
 
-  test('asynchronous passing test', async (t) => {
-    // This test passes because the Promise returned by the async
-    // function is not rejected.
-    assert.strictEqual(1, 1);
-  });
+// test('synchronous passing test', (t) => {
+//     // This test passes because it does not throw an exception.
+//     assert.strictEqual(1, 1);
+//   });
 
-  test('asynchronous failing test', async (t) => {
-    // This test fails because the Promise returned by the async
-    // function is rejected.
-    assert.strictEqual(1, 2);
-  });
+//   test('synchronous failing test', (t) => {
+//     // This test fails because it throws an exception.
+//     assert.strictEqual(1, 2);
+//   });
 
-  test('failing test using Promises', (t) => {
-    // Promises can be used directly as well.
-    return new Promise((resolve, reject) => {
-      setImmediate(() => {
-        reject(new Error('this will cause the test to fail'));
-      });
-    });
-  });
+//   test('asynchronous passing test', async (t) => {
+//     // This test passes because the Promise returned by the async
+//     // function is not rejected.
+//     assert.strictEqual(1, 1);
+//   });
 
-  test('callback passing test', (t, done) => {
-    // done() is the callback function. When the setImmediate() runs, it invokes
-    // done() with no arguments.
-    setImmediate(done);
-  });
+//   test('asynchronous failing test', async (t) => {
+//     // This test fails because the Promise returned by the async
+//     // function is rejected.
+//     assert.strictEqual(1, 2);
+//   });
 
-  test('callback failing test', (t, done) => {
-    // When the setImmediate() runs, done() is invoked with an Error object and
-    // the test fails.
-    setImmediate(() => {
-      done(new Error('callback failure'));
-    });
-  });
+//   test('failing test using Promises', (t) => {
+//     // Promises can be used directly as well.
+//     return new Promise((resolve, reject) => {
+//       setImmediate(() => {
+//         reject(new Error('this will cause the test to fail'));
+//       });
+//     });
+//   });
+
+//   test('callback passing test', (t, done) => {
+//     // done() is the callback function. When the setImmediate() runs, it invokes
+//     // done() with no arguments.
+//     setImmediate(done);
+//   });
+
+//   test('callback failing test', (t, done) => {
+//     // When the setImmediate() runs, done() is invoked with an Error object and
+//     // the test fails.
+//     setImmediate(() => {
+//       done(new Error('callback failure'));
+//     });
+//   });
