637090960da657280da7b0f6ad7782893dbe10a0
Steven R. Loomis <srl295@gmail.com>
2023-03-03 18:53:24 -0600

    spiff it up a bit

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -9,8 +9,22 @@
   <p>
     Sample app for
     <a href="https://docs.google.com/document/d/1FoV37ymU3VR0bI_V8H4cnN0Q5gkorTJa2hWHpw3NT6w/edit#">CLDR keyboard
-      Transform Proposal</a>
+      Transform Proposal</a> (which see)
   </p>
+
+  <h3> HOW TO USE </h3>
+
+  <ol>
+    <li>View or edit the XML source, sample files <a
+        href="https://github.com/srl295/cldr-kbd-transform/tree/master/test/xml">here</a></li>
+    <li>Edit the <b>Text Source</b> field with your input text. <br />These might help: (<a
+        href="https://r12a.github.io/uniview/#title">uniview</a> | <a
+        href="https://util.unicode.org">util.unicode.org</a>) </li>
+    <li>The 'Target' field will show the output. If it doesn’t say DONE under the target,
+    there may be an error somewhere.</li>
+  </ol>
+
+  <hr />
 
   <h2>
     By Steven R. Loomis
@@ -24,5 +38,4 @@
     <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
   </h3>
 
-  <a href="https://github.com/srl295/cldr-kbd-transform">Fork me on GitHub!</a>
-</template>
+  <a href="https://github.com/srl295/cldr-kbd-transform">Fork me on GitHub!</a></template>
